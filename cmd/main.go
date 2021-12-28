16985916822b119acee55f41c0d60a9e82cecad7
Dylan Johnson <dylanjohnson@vista.local>
2021-12-28 14:01:56 -0700

    fixing flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,46 +2,26 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/helvellyn-io/container-go/src/docker"
 	"github.com/helvellyn-io/container-go/src/git"
 )
 
-var getBuildArtifacts *string
-var buildContainer *string
-var flagSlice []string
+var operation *string
 
 func init() {
-	getBuildArtifacts = flag.String("ga", "get-artifacts", "Gets build artifacts.")
-	buildContainer = flag.String("bc", "build-container", "Builds a container using artifacts.")
+	operation = flag.String("operation", "definition", "Gets build artifacts.")
 
 }
 
 func main() {
 
 	flag.Parse()
-	flagSlice = append(flagSlice, *getBuildArtifacts)
-	flagSlice = append(flagSlice, *buildContainer)
 
-loop:
-	for _, v := range flagSlice {
-
-		switch v {
-
-		case "get-artifacts":
-			//copy build artifacts from Git.
-			git.GetBuildArtifacts()
-			break loop
-
-		case "build-container":
-			//build container using build artifacts
-			docker.BuildImage(docker.InitDockerBuild())
-			break loop
-
-		default:
-			fmt.Println("Please supply an operation: get-artifacts , build-container")
-			break loop
-		}
+	if *operation == "get-artifacts" {
+		git.GetBuildArtifacts()
+	} else if *operation == "build-container" {
+		docker.BuildImage(docker.InitDockerBuild())
 	}
+
 }
